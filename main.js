291ce443f285291c974bbe574c26b88274fd48fe
Anant <anantcode@gmail.com>
2021-08-08 15:08:47 +0530

    Final - renders every second, calculates next no. of sequence every 10ms. (infinite loop alongside setInterval was causing setInterval to NOT properly

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 const printer = require("./printer");
+setInterval(function () {
+    console.clear();
+    printer.print();
+}, 1000); //renders every second.
 
 var args = process.argv.slice(2);
 let num = args;
-//process.stdout.write("Hi ");
+// //process.stdout.write("Hi ");
 
 start(num);
 
 function start(n) {
-    while (1) {
+    setInterval(function () {
         if (n % 2 == 0) {
             n = n / 2;
         } else {
             n = 3 * n + 1;
         }
-        printer.print(n);
-    }
+        printer.updateSeq(n);
+    }, 10); //calculates next no. in sequence every 10ms
 }
--- printer.js
+++ printer.js
@@ -4,27 +4,19 @@ class Printer {
         this.page = {};
     }
 
-    print(n) {
-        this.updatePage(n);
-        this.renderPage();
-    }
-
-    updatePage(n) {
+    updateSeq(n) {
         if (this.page[n]) {
             this.page[n] = this.page[n] + 1;
         } else {
             this.page[n] = 1;
         }
-
-        console.log(`Page Updated! with ${n}`);
     }
 
-    renderPage() {
+    print() {
         console.clear();
         for (const property in this.page) {
             console.log(`${property}: ${this.page[property]}`);
         }
-        console.log("Page Rendered!");
     }
 }
 
